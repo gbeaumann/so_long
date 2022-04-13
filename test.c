e0f51a6209d7386b43832cb54020fe9456115256
Gregoire Beaumann <gbeauman@c1r2s3.42lausanne.ch>
2022-04-13 16:16:53 +0200

    loop

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include	<stdio.h>
 #include	"so_long.h"
+#include	<stdlib.h>
+#include	<unistd.h>
 
 typedef	struct	s_data
 {
@@ -8,6 +10,10 @@ typedef	struct	s_data
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
+	void	*mlx;
+	void	*mlx_win;
+	int		x;
+	int		y;
 }			t_data;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -17,44 +23,102 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
 }
+int clic_close(t_data *img)
+{
+	mlx_destroy_window(img->mlx, img->mlx_win);
+	exit (0);
+}
+int	ft_close(int keycode, t_data *img)
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_window(img->mlx, img->mlx_win);
+		exit(0);
+	}
+	return (0);
+}
+
+/*int	key_hook(int keycode, t_data *img)
+{
+	printf("%d\n", keycode);
+	return (0);
+}*/
+
+int	render_next_frame(t_data *img)
+{
+	static int	check = 0;
+	int			x;
+	int			y;
+
+	if (img->mlx != NULL && check == 0)
+	{
+		y = 0;
+		while (y < 600)
+		{
+			x = 0;
+			while (x < 800)
+			{
+				my_mlx_pixel_put(img, x, y, 0x00ff0000);
+				x++;
+			}
+			y++;
+		}
+		mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
+		check = 1;
+		sleep(1);
+		return (0);
+	}
+	if (img->mlx != NULL && check == 1)
+	{
+		img->y = 0;
+		while (img->y < 600)
+		{
+			img->x = 0;
+			while (img->x < 800)
+			{
+				my_mlx_pixel_put(img, img->x, img->y, 0x0000ff00);
+				img->x++;
+			}
+			img->y++;
+		}
+		mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
+		check = 2;
+		sleep(1);
+		return (0);
+	}
+	if (img->mlx != NULL && check == 2)
+	{
+		img->y = 0;
+		while (img->y < 600)
+		{
+			img->x = 0;
+			while (img->x < 800)
+			{
+				my_mlx_pixel_put(img, img->x, img->y, 0x000000ff);
+				img->x++;
+			}
+			img->y++;
+		}
+		mlx_put_image_to_window(img->mlx, img->mlx_win, img->img, 0, 0);
+		check = 0;
+		sleep(1);
+		return (0);
+	}
+	return (0);
+}
 
 int	main(void)
 {
-	void	*mlx;
 	t_data	img;
-	void	*mlx_win;
-	int		x;
-	int		y;
 
-	x = 50;
-	y = 50;
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 800, 600, "test");
-	img.img = mlx_new_image(mlx, 800, 600);
+	img.mlx = mlx_init();
+	img.mlx_win = mlx_new_window(img.mlx, 800, 600, "test");
+	img.img = mlx_new_image(img.mlx, 800, 600);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	while (y < 100)
-	{
-		my_mlx_pixel_put(&img, x, y, 0x0033FFFF);
-		mlx_put_image_to_window(mlx, mlx_win, img.img, 50, 50);
-		y++;
-	}
-	/*while (x < 100)
-	{
-		my_mlx_pixel_put(&img, x, y, 0x00ff0000);
-		mlx_put_image_to_window(mlx, mlx_win, img.img, 200, 50);
-		x++;
-	}
-	while (y > 50)
-	{
-		my_mlx_pixel_put(&img, x, y, 0x0033FF00);
-		mlx_put_image_to_window(mlx, mlx_win, img.img, 50, 200);
-		y--;
-	}
-	while (x > 50)
-	{
-		my_mlx_pixel_put(&img, x, y, 0x009900CC);
-		mlx_put_image_to_window(mlx, mlx_win, img.img, 200, 200);
-		x--;
-	}*/
-	mlx_loop(mlx);
+	mlx_loop_hook(img.mlx, render_next_frame, &img);
+	//mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
+	mlx_hook(img.mlx_win, 2, 1L<<0, ft_close, &img);
+	mlx_hook(img.mlx_win, 17, 1L<<5, clic_close, &img);
+	//mlx_key_hook(img.mlx_win, key_hook, &img);
+	mlx_loop(img.mlx);
 }
